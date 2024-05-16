66311b9a3897341d671ff26b7c0014ea9a5e3f8e
prajithravisankar <99565302+prajithravisankar@users.noreply.github.com>
2024-05-16 13:05:58 -0700

    finished sliding window problem
    find the maximum sum in a sliding window  (brute force method)

--- arrayHomework3_fixedSlidingWindowMaxSumWindow.cpp
+++ arrayHomework3_fixedSlidingWindowMaxSumWindow.cpp
@@ -1,42 +1,42 @@
 #include <iostream>
 
-
 using namespace std;
 
 int main() {
 
-    // 0 1 2 - 2 3  -> a sub array of index 3 4 5 because sum of values at 3 , 4 , 5 = max sum in the window
+    // IDEA: using brute force, find the window with max sum, and make a note of the index,
+    // from this we can get the window with max sum in an array. There is efficient way to do this.
 
-    int N, K;
-    cout << "enter N: ";
+    cout << "enter number of elements in array: ";
+    int N;
     cin >> N;
-
-    cout << "enter window size: ";
-    cin >> K;
-
-    int arr[N] = {0}, sub[K] = {0};
+    int arr[N] = {0};
 
     for (int i = 0; i < N; i++) {
-        cout << "enter input value to array: ";
+        cout << "arr[" << i << "]: ";
         cin >> arr[i];
     }
 
-    int i = 0;
-    int j = i + 1;
-    int k = j + 1;
-    for (; k < N; i++, j++, k++) {
-        sub[i] = arr[i] + arr[j] + arr[k];
-    }
+    cout << "enter window size: ";
+    int window;
+    cin >> window;
 
-    int maxIndex = 0;
-    for (int i = 1; i < N; i++) {
-        if (sub[maxIndex] < sub[i]) {
-            maxIndex = i;
+    int dummyMax = -200000, slidingIndex = 0;
+
+    for (int i = 0; i < N - window + 1; i++) {
+        int sum = 0;
+        for (int j = 0; j < window; j++) {
+            sum += arr[i + j];
+        }
+
+        if (sum > dummyMax) {
+            dummyMax = sum;
+            slidingIndex = i;
         }
     }
 
-    cout << "max window is : " << maxIndex << " " << maxIndex + 1 << " " << maxIndex + 2 << endl;
-    cout << "max sum is : " << arr[maxIndex] + arr[maxIndex + 1] + arr[maxIndex + 2] << endl;
+    cout << "the max sum value is : " << dummyMax << " starting from : " << slidingIndex << " to ";
+    cout << slidingIndex + window - 1 << endl;
 
     return 0;
 }
