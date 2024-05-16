a25e3d031604b5ed0cbfdeea4a6937591321388d
prajithravisankar <99565302+prajithravisankar@users.noreply.github.com>
2024-05-16 13:43:51 -0700

    counting increasing sub array for the given array
    didn't use any nested loop in this commit

--- arrayHomework3_countIncreasingSubarrays.cpp
+++ arrayHomework3_countIncreasingSubarrays.cpp
@@ -3,27 +3,38 @@
 using namespace std;
 
 int main() {
-    cout << "enter N:";
-    int N, sum;
-    cin >> N;
 
-    sum = N;
+    // IDEA: to find the number of increasing sub arrays for a given array,
+    // keep a side track of a counter where, if a current number is > than previous number
+    // count++ and add count to the number of items in the array (size of the array). If the
+    // current number in the iteration is less than the previous number, count--. Keep iterating
+    // through the array, and we will have the number of increasing sub arrays.
+
+    cout << "enter N < 200: ";
+    int N;
+    cin >> N;
     int arr[N];
 
-    int j = 1;
     for (int i = 0; i < N; i++) {
-        cout << "enter value to put in arr: ";
+        cout << "enter arr[" << i << "]: ";
         cin >> arr[i];
+    }
 
-        if (i > 0) {
-            if (arr[i] > arr[i - 1]) {
-                sum += j;
-                j++;
-            }
+    int sum = N;
+    int cnt = 0; // counter
+    for (int i = 1; i < N; i++) {
+        if (arr[i] > arr[i - 1]) {
+            // if current number is greater than previous number
+            cnt++;
+            sum += cnt;
+        }
+        else if (arr[i] < arr[i - 1]) {
+            // if current number is less than previous number
+            cnt--;
         }
     }
 
-    cout << "total number of increasing sub arrays are: " << sum;
+    cout << "number of increasing sub arrays are: " << sum;
 
     return 0;
 }
